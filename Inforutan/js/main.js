be2eed54aedde99d71de2080c2bbad2fe087d203
eriklofblad <erikhinnerdal@gmail.com>
2018-08-08 22:18:01 +0200

    tillagd php proxy dock ej implementerad ännu

--- Inforutan/js/main.js
+++ Inforutan/js/main.js
@@ -1,36 +1,10 @@
 $(document).ready(function(){
     $.ajaxSetup({ cache: false });
-    
-    /*
-    //Test för att parsa komplexa JSON
-    $.getJSON('telefonbok_2.json', function(json) {
-        $.each(json, function(key, value){
-            $('#home').append('<h3>'+key+'</h3>')
-            $.each(value, function(key2, value2){
-                $('#home').append('<p>Phonenumber: '+value2.name+'</p>')
-            });
-        });
-    });
-    */
 
     $(document).tooltip({
         container: 'body',
         selector: '[data-toggle]'
     });
-    
-    // $("#driftinfo-body").load("Akut_driftinformation-Inuti_2.htm")
-
-    /*
-    $.ajax({
-        url: 'https://eriklofblad.github.io/Inforutan/driftinfo_3.htm',
-        type: 'GET',
-        crossDomain: true,
-        dataType: 'html',
-        success: function(data){
-            $('#driftinfo-body').append(data);
-        }
-    });
-    */
 
     $.ajax({
         url: 'https://eriklofblad.github.io/Inforutan/driftinfo_3.htm',
@@ -50,45 +24,6 @@ $(document).ready(function(){
             $(".list-group-item a").addClass('text-danger');
         }
     });
-
-    $.ajax({
-        url: 'https://schema.medinet.se/ksneurorad/schema/neuron/language/se',
-        success: function(data){
-            var oncalldoctor = $(data).find("#day-79-2018-08-06");
-            $('#driftinfo-body').append(oncalldoctor);
-        }
-
-    });
-
-    /*
-    $.get('Akut_driftinformation-Inuti_2.htm', function(data){
-        console.log("AJAX lyckad");
-        //var news_selector = $('.news');
-        // var news_elements = $(".news", data);
-        var parser = new DOMParser();
-        var news_elements = parser.parseFromString(data, "text/html");
-        console.log(news_elements);
-        // news_elements.innerHTML = data;
-        // document.querySelector("#driftinfo-body").innerHTML = news_elements.getElementsByClassName("news").innerHTML;
-        console.log(news_elements.getElementsByClassName("news"));
-
-        $('#driftinfo-body').append(news_elements);
-
-        // document.getElementById("driftinfo-body").innerHTML = news_elements2.innerHTML;
-        // $(news_elements).addClass('list-group').removeClass('news');
-        // $('#driftinfo-body').append(news_elements);
-        // $('#driftinfo-body').html(news_elements2);
-        $(".list-group li").addClass('list-group-item bg-warning');
-        $(".list-group-item a").addClass('text-danger');
-        console.log("efter ajax processning");
-        
-    });
-
-    */
-
-
-
-
 
     $('#searchNumber').keyup(function(){
         $('#numberList').html('');
