cbaf23cdff5e1a603cec458d6eb14d461e9d025e
Erik Löfblad <erikhinnerdal@gmail.com>
2018-09-30 22:18:52 +0200

    Uppdateringar av telefonsök

--- Inforutan/js/phoneSearch.js
+++ Inforutan/js/phoneSearch.js
@@ -14,13 +14,13 @@ $(document).ready(function(){
 		if (searchField != '') {
 			var expression = new RegExp(searchField, 'i');
 			
-            $('#numberList').html('<thead><tr><th>Namn</th><th>Nummer</th><th>Roll</th><th>FO</th></thead>');
+            $('#numberList').html('<thead><tr><th>Namn</th><th>Nr.</th><th>Roll</th><th>FO</th></thead>');
             $('#numberList').append('<tbody>');
             $.each(jsondata, function (key1, value1) {
                 $.each(value1, function (key, value) {
                     if (value.name.search(expression) != -1) {
                         //här väljer vi vilka noder som ska visas
-                        $('#numberList').append('<tr><th id="erikstest" scope="row" data-toggle="tooltip" title="' + value.description + '">' + value.name + '</th><td>' + value.phonenumber + '</td><td>' + value.type + '</td><td>' + value.organisation + '</td></tr>');
+                        $('#numberList').append('<tr class="p-1"><th id="erikstest" class="p-1" scope="row" data-toggle="tooltip" title="' + value.description + '">' + value.name + '</th><td class="p-1">' + value.phonenumber + '</td><td class="p-1">' + value.type + '</td><td class="p-1">' + value.organisation + '</td></tr>');
                     }
                 });
             });
